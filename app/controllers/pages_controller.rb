b445b5486e46f2e36176062d47b4f764a372dc83
Claudia Terpstra <claudia_terpstra@hotmail.com>
2017-09-04 20:50:45 +0100

    added more pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def koor
+  end
+
+  def piano
+  end
+
+  def didjeridu
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
-  get 'about', to: 'pages#about'
-
+  get 'koor', to: 'pages#koor'
+  get 'piano', to: 'pages#piano'
+  get 'didjeridu', to: 'pages#didjeridu'
+  get 'contact', to: 'pages#contact'
 end
