2f1300d44a473f8fa45e857d0c8e1d1abe63611e
Claudia Terpstra <claudia_terpstra@hotmail.com>
2017-09-11 21:52:58 +0100

    changed didjeridu page

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < MailForm::Base
-  attribute :name,      :validate => true
+  attribute :naam,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
-  attribute :message,   :validate => true
+  attribute :bericht,   :validate => true
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
   def headers
